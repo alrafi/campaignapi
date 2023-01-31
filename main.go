d401634a90766db88bcabccae4b67766c5dc4e66
alrafi <hafisalrafi@gmail.com>
2023-01-31 22:06:51 +0700

    Get data on table user

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"campaignapi/user"
 	"fmt"
 	"log"
 
@@ -11,11 +12,19 @@ import (
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:Bismillah123_@tcp(127.0.0.1:3306)/dbcampaign?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is good...")
+
+	var users []user.User
+
+	db.Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name, "|", user.Email)
+	}
 }
