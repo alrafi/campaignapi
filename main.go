5f854f60c4236d326c7464314345dd37ba422375
alrafi <hafisalrafi@gmail.com>
2023-02-02 11:50:22 +0700

    Add user via service

--- main.go
+++ main.go
@@ -4,20 +4,12 @@ import (
 	"campaignapi/user"
 	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:Bismillah123_@tcp(127.0.0.1:3306)/dbcampaign?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -28,8 +20,25 @@ func handler(c *gin.Context) {
 
 	fmt.Println("Connection to database is good...")
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
 
-	c.JSON(http.StatusOK, users)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Daniel Ortega"
+	userInput.Email = "daniel@ortega.com"
+	userInput.Occupation = "Programmer"
+	userInput.Password = "qwerty"
+
+	userService.RegisterUser(userInput)
+
+	// router := gin.Default()
+	// router.GET("/", handler)
+	// router.Run()
 }
+
+// func handler(c *gin.Context) {
+// 	var users []user.User
+// 	db.Find(&users)
+
+// 	c.JSON(http.StatusOK, users)
+// }
