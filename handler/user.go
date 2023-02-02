7a358b94d87bc92fc4c4d922fb2f3ff40ddbc86b
alrafi <hafisalrafi@gmail.com>
2023-02-02 16:35:35 +0700

    Add user formatter

--- handler/user.go
+++ handler/user.go
@@ -24,11 +24,13 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	response := helper.APIResponse("User has been registered", 200, "success", user)
+	formatter := user.FormatUser(newUser, "token")
+
+	response := helper.APIResponse("User has been registered", 200, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
