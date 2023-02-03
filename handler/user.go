4f6be4f71d4333f09288ca711363d59d897f9d1f
alrafi <hafisalrafi@gmail.com>
2023-02-03 16:08:55 +0700

    Add login user handler

--- handler/user.go
+++ handler/user.go
@@ -40,3 +40,28 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.APIResponse("User has been registered", 200, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) LoginUser(c *gin.Context) {
+	var input user.LoginUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		errResponse := helper.APIResponse("Login failed", http.StatusInternalServerError, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, errResponse)
+		return
+	}
+
+	loginUser, err := h.userService.LoginUser(input)
+	if err != nil {
+		errResponse := helper.APIResponse("Login failed", http.StatusInternalServerError, "error", nil)
+		c.JSON(http.StatusBadRequest, errResponse)
+		return
+	}
+
+	formatter := user.FormatUser(loginUser, "token")
+
+	response := helper.APIResponse("Login succeed", 200, "success", formatter)
+	c.JSON(http.StatusOK, response)
+}
